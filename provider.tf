821c8239044378979a06dcbfd805fa279f089f0d
Mike <ueberhund@hotmail.com>
2020-02-20 19:16:00 -0700

    Add files via upload

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+    backend "s3" {
+      encrypt = true
+      bucket = "aws-gmike-terraform-state"
+      dynamodb_table = "terraform-state"
+      key = "path/path/terraform.tfstate"
+      region = "us-east-1"
+  }
+}
